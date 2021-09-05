da941692d7cd4edc296fbc44e8e9357bd52dca36
JY <15048471585@163.com>
2021-09-05 17:12:56 +0800

    Nothing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 
-function App() {
-  return (
-    <div>
-      Hello world
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        app-js
+      </div>
+    );
+  }
 }
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from 'react';//确保JSX语法的解析
 import ReactDOM from 'react-dom';
+
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <div>
     <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
